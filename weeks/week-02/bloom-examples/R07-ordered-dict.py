a8b4a0d8c2dbc648b91db9c4b27706bc3b49bc66
faxyu729 <d14405020@ems.npu.edu.tw>
2026-03-19 19:50:14 +0800

    Remove week-02 and week-03 directories from main
    These were accidentally merged from PR #104 but should not be part of the main branch content.

--- weeks/week-02/bloom-examples/R07-ordered-dict.py
+++
@@ -1,8 +0,0 @@
-# R7. OrderedDict（1.7）
-
-from collections import OrderedDict
-import json
-
-d = OrderedDict()
-d['foo'] = 1; d['bar'] = 2
-json.dumps(d)
--- weeks/week-02/bloom-examples/R11-slice.py
+++
@@ -1,6 +0,0 @@
-# R11. 命名切片 slice（1.11）
-
-record = '....................100 .......513.25 ..........'
-SHARES = slice(20, 23)
-PRICE = slice(31, 37)
-cost = int(record[SHARES]) * float(record[PRICE])
